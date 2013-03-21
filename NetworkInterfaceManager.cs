4719a7acc5ef4e1ca717f7b46a159a7d5f06089a
jinyu.yin <jinyu.yin@dianping.com>
2013-03-21 14:37:12 +0800

    Add NetworkInterfaceManager class.

---
+++ NetworkInterfaceManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace Com.Dianping.Cat
+{
+    using System.Net;
+
+    public class NetworkInterfaceManager
+    {
+        public static string GetLocalHostName()
+        {
+            return Dns.GetHostName();
+        }
+
+        public static string GetLocalHostAddress()
+        {
+            IPHostEntry host = Dns.GetHostEntry(GetLocalHostName());
+
+            foreach (IPAddress ip in host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork))
+            {
+                return ip.ToString();
+            }
+
+            throw new NotSupportedException("No IP address found");
+        }
+
+        public static byte[] GetAddressBytes()
+        {
+            IPHostEntry host = Dns.GetHostEntry(GetLocalHostName());
+
+            foreach (IPAddress ip in host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork))
+            {
+                return ip.GetAddressBytes();
+            }
+
+            throw new NotSupportedException("No IP address found");
+        }
+    }
+}
