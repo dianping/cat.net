7bcbe22935f1503720b0da667ab71f36ab1f20de
jinyu.yin <jinyu.yin@dianping.com>
2013-03-21 11:59:32 +0800

    Add DefaultEvent,DefaultHeartbeat,DefaultTransaction class.

---
+++ Message/Internals/DefaultEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Com.Dianping.Cat.Message.Internals
+{
+    using Message;
+    using System;
+
+    public class DefaultEvent : AbstractMessage, IEvent
+    {
+        public DefaultEvent(String type, String name) : base(type, name)
+        {
+        }
+    }
+}
---
+++ Message/Internals/DefaultHeartbeat.cs
@@ -0,0 +1,12 @@
+﻿namespace Com.Dianping.Cat.Message.Internals
+{
+    using Message;
+    using System;
+
+    public class DefaultHeartbeat : AbstractMessage, IHeartbeat
+    {
+        public DefaultHeartbeat(String type, String name) : base(type, name)
+        {
+        }
+    }
+}
